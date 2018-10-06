1dfadbde6695b09c2e30976bfbcb757cbe42a1fe
e440 <jvejar1@miuandes.cl>
2018-10-06 18:10:13 -0300

    users: generate token when created mailer:acvtivation email/welcome email with view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_create :set_access_token
+  def self.random_string(len)
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    newpass = ""
+    1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+    return newpass
+  end
+
+  private
+  def set_access_token
+    self.auth_token = generate_token
+  end
+
+  def generate_token
+    loop do
+      token = SecureRandom.hex(10)
+      break token unless User.where(auth_token:token).exists?
+    end
+  end
 end
